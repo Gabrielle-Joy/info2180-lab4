712b7986185b60eaf6752bebf63f903a1da59423
Gabrielle-Joy <gabzi.contact@gmail.com>
2019-10-25 18:43:30 -0500

    fix colour and add hover effect

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,14 +3,17 @@
 
 
 const ready = () => {
+
+const boxElementsArray = document.getElementById("board").querySelectorAll("div");
+
 //  Layout the board 
-document.getElementById("board").querySelectorAll("div").forEach(box => box.classList.add("square"));
+boxElementsArray.forEach(box => box.classList.add("square"));
 
 // Add an X or O to a square when clicked 
 const sqArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let i = 0;
 let turn = 0;
-document.getElementById("board").querySelectorAll("div").forEach(box => {
+boxElementsArray.forEach(box => {
     box.setAttribute("id", i);
     i++;
     box.addEventListener("click", () => {
@@ -18,15 +21,23 @@ document.getElementById("board").querySelectorAll("div").forEach(box => {
         if (turn%2===0) {
             box.textContent = 'X';
             sqArray[boxID] = 1;
+            box.classList.add("X")
         } else {
             box.textContent = 'O';
             sqArray[boxID] = -1;
+            box.classList.add("O")
         } turn++;
     });
 });
 
 /* Change the style when you move your
 mouse over a square */ 
+boxElementsArray.forEach(box => box.addEventListener("mouseenter", () => {
+    box.classList.add("hover");
+}));
+boxElementsArray.forEach(box => box.addEventListener("mouseout", () => {
+    box.classList.remove("hover");
+}));
 
 
 /*Check for the winner and update the
