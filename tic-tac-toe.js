a48bce54e795873312270c24efc20f95292d1163
Gabrielle-Joy <gabzi.contact@gmail.com>
2019-10-25 17:11:40 -0500

    click for x or o

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,11 +6,24 @@ const ready = () => {
 //  Layout the board 
 document.getElementById("board").querySelectorAll("div").forEach(box => box.classList.add("square"));
 
-
-//document.getElementsByTagName("div").getAttribute("square");
-
 // Add an X or O to a square when clicked 
-
+const sqArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let i = 0;
+let turn = 0;
+document.getElementById("board").querySelectorAll("div").forEach(box => {
+    box.setAttribute("id", i);
+    i++;
+    box.addEventListener("click", () => {
+        boxID = box.getAttribute("id");
+        if (turn%2===0) {
+            box.textContent = 'X';
+            sqArray[boxID] = 1;
+        } else {
+            box.textContent = 'O';
+            sqArray[boxID] = -1;
+        } turn++;
+    });
+});
 
 /* Change the style when you move your
 mouse over a square */ 
