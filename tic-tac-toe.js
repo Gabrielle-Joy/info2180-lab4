f668a2d5f8131d1a3ca4b5d99fe39ed192f9bf47
Gabrielle-Joy <gabzi.contact@gmail.com>
2019-10-25 16:09:52 -0500

    add squares

---
+++ tic-tac-toe.js
@@ -0,0 +1,29 @@
+
+// Gabrielle Joy Higgins 
+
+
+const ready = () => {
+//  Layout the board 
+document.getElementById("board").querySelectorAll("div").forEach(box => box.classList.add("square"));
+
+
+//document.getElementsByTagName("div").getAttribute("square");
+
+// Add an X or O to a square when clicked 
+
+
+/* Change the style when you move your
+mouse over a square */ 
+
+
+/*Check for the winner and update the
+status*/
+
+
+// Restart the game
+
+
+// Disallow Cheating
+}
+
+document.addEventListener("DOMContentLoaded", ready);
